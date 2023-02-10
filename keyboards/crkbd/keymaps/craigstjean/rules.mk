fa90b25cce112d38173c81bc02aeee68ad2cf4a2
Craig St. Jean <craigstjean@gmail.com>
2023-02-09 22:45:50 -0500

    Initial Corne craigstjean configuration

--- keyboards/crkbd/keymaps/craigstjean/rules.mk
+++ keyboards/crkbd/keymaps/craigstjean/rules.mk
@@ -1,5 +1,6 @@
-MOUSEKEY_ENABLE = yes    # Mouse keys
-RGBLIGHT_ENABLE = yes    # Enable WS2812 RGB underlight.
-OLED_ENABLE     = no
-OLED_DRIVER     = SSD1306
-LTO_ENABLE      = yes
+MOUSEKEY_ENABLE  = no     # Mouse keys
+BACKLIGHT_ENABLE = no     # Enable keybaord backlight functionality
+RGBLIGHT_ENABLE  = yes    # Enable WS2812 RGB underlight.
+OLED_ENABLE      = no
+LTO_ENABLE       = yes
+VIA_ENABLE       = yes    # Enable VIA
